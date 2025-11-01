ec6b727963200da8eaadb41ccf267a353a4d11b5
mrjohnlowlevel <johnfree751@gmail.com>
2025-11-01 15:08:31 +0800

    added basic abstraction over syscall

--- src/main.asm
+++ src/main.asm
@@ -1,16 +1,29 @@
-section .data
-        test_str: db "Hello, dev branch!",10
-        testl: equ $-test_str
+section .bss
+        uinp: resb 255  ; 8-bit int limit, i don't know what the fuck im doing
 
 section .text
         global _start
 
-_start:
+get_user_input:
+        mov rax, 0
+        mov rdi, 0
+        syscall
+        ret
+
+print:
         mov rax, 1
         mov rdi, 1
-        mov rsi, test_str
-        mov rdx, testl
         syscall
+        ret
+
+_start:
+        mov rsi, uinp
+        mov rdx, 255
+        call get_user_input
+
+        mov rsi, uinp
+        mov rdx, 255
+        call print
 
         mov rax, 60
         xor rdi, rdi
